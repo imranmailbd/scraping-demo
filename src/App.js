7aa0783ac9dacc64dbac92023b1f6e189befdd9f
Muhammed Imran <imranmailbd@gmail.com>
2019-12-23 19:03:08 +0600

    parser ok

--- src/App.js
+++ src/App.js
@@ -1,64 +1,113 @@
 import React, { Component } from "react";
 import rp from "request-promise";
 import cheerio from "cheerio";
-
 import "./App.css";
 
 
 
 class App extends Component {
-  state = { names: [] };
 
-  componentDidMount() {
+  state = { scrapdata: [] };
+
+  componentDidMount() {    
     // use the request-promise library to fetch the HTML from pokemon.org
     rp("https://www.marketbeat.com/stocks/directory/A/")
       .then(html => {
-        let names = [];
+        let scrapdata = [];
         let $ = cheerio.load(html);
 
-        // find what element ids, classes, or tags you want from opening console in the browser
-        // cheerio library lets you select elements similar to querySelector
-        //$("#monsters-list li span").each(function(i, element) {
         $("tr").each(function(i, element) {
-          let name = $(this)
+          let scraprow = $(this)
             .prepend()
             .html();
-          names.push(name);
+          scrapdata.push(scraprow);
+
         });
 
-        this.setState({ names });
-        console.log({ names });
+        this.setState({ scrapdata });
+        console.log({ scrapdata });        
+
       })
       .catch(function(err) {
         console.log("crawl failed");
       });
-
-
-
-
   }
 
+
   render() {
-    return (
+    
+    return (     
 
-     
-
-      <div>
-        <ul>
-          {this.state.names.map(name => {
-
-             var HTMLParser = require('fast-html-parser');
-             var test = name;
-       var root = HTMLParser.parse(test);
-      console.log(root.firstChild.structure);
-
-            return <li key={name}>{name}</li>;  
+              <div>
+                <ul>
+                  {this.state.scrapdata.map(scraprow => {
 
 
+                    var scrapStrObj = {};
+                                       
+                    //######## DOM Parse Machanism ########
+                    const hpq = require('hpq');
+                    const document = hpq.parse( scraprow, {
+                        src: hpq.attr( 'img', 'src' ),
+                        href: hpq.attr( 'a', 'href' ),
+                        company: hpq.query( '.ticker-area', hpq.text() ),
+                        
+                    } );
 
-          })}
-        </ul>
-      </div>
+                    scrapStrObj.src = document.src;
+                    scrapStrObj.href = document.href;
+                    scrapStrObj.company = document.company;
+
+
+                    //console.log(document.src);
+                    //console.log(document.href);
+                    //console.log(document.company); 
+                    //########################################
+
+                    //####### Dirty Hand Machanism #####
+                    var str = scraprow;
+                    var res = str.split("</td>");                   
+                    var rawbuy=res[3];
+                    var rawsell=res[4]; 
+                    rawbuy = String(rawbuy); 
+                    rawsell = String(rawsell);                   
+
+                    if ((rawbuy===null) || (rawbuy==='')){
+                      //console.log('none');
+                      scrapStrObj.buy = null; 
+                    } 
+
+                    if ((rawbuy!==null) || (rawbuy!=='')){
+                        const stripHtml = require("string-strip-html");
+                        //console.log(stripHtml(rawbuy));  
+                        scrapStrObj.buy = stripHtml(rawbuy);                    
+                    }
+
+                    if ((rawsell===null) || (rawsell==='')){
+                        //console.log('none');
+                        scrapStrObj.sell = null; 
+                    } 
+
+                    if ((rawsell!==null) || (rawsell!=='')){
+                        const stripHtml = require("string-strip-html");
+                        //console.log(stripHtml(rawsell));  
+                        scrapStrObj.sell = stripHtml(rawsell);
+                    }  
+
+
+                    //########################################### 
+
+
+            
+
+
+                    return <li key={scraprow}><ul><li>{scrapStrObj.company}</li><li>{scrapStrObj.buy}</li><li>{scrapStrObj.sell}</li><li>{scrapStrObj.src}</li><li>{scrapStrObj.href}</li></ul></li>;  
+
+
+
+                  })}
+                </ul>
+              </div>
     );
   }
 }
